abaebff7bf58f6c243654f96fd0a20179678045c
Dmitry <kudim@inbox.ru>
2026-05-04 20:58:59 +0300

    feat: add preloader

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import Search from './components/Search';
 import MovieList from './components/MovieList';
 import LoadMore from './components/LoadMore';
+import Preloader from './components/Preloader';
 
 const API_KEY = '61ba9e64';
 class App extends React.Component {
@@ -12,9 +13,11 @@ class App extends React.Component {
     localSearch: localStorage.getItem('search') || 'movie',
     page: 1,
     isLoadMore: true,
+    isLoading: false,
   };
 
   async componentDidMount(): Promise<void> {
+    this.setState({ isLoading: true });
     if (localStorage.getItem('search')) {
       this.setState({ isLoadMore: false });
     }
@@ -24,12 +27,15 @@ class App extends React.Component {
       );
       const data = await response.json();
       this.setState({ movies: data.Search });
+      this.setState({ isLoading: false });
     } catch (err) {
       console.error(err);
+      this.setState({ isLoading: false });
     }
   }
 
   searchMovies = async (search: string, page = 1) => {
+    this.setState({ isLoading: true });
     try {
       const response = await fetch(
         `https://www.omdbapi.com/?apikey=${API_KEY}&s=${search}&page=${page}`,
@@ -37,11 +43,13 @@ class App extends React.Component {
       if (response.ok) {
         const data = await response.json();
         this.setState({ movies: data.Search });
+        this.setState({ isLoading: false });
       } else {
         console.log(response.json());
       }
     } catch (err) {
       console.error(err);
+      this.setState({ isLoading: false });
     }
   };
 
@@ -61,7 +69,7 @@ class App extends React.Component {
       <div className="container">
         <Header />
         <Search searchMovies={this.searchMovies} setPage={this.setPage} />
-        <MovieList movies={this.state.movies} />
+        {this.state.isLoading ? <Preloader /> : <MovieList movies={this.state.movies} />}
         {this.state.isLoadMore && <LoadMore loadMore={this.loadMore} page={this.state.page} />}
       </div>
     );
