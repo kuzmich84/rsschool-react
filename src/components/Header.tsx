18a15a034628bd9fde109b6a229bba119ddec76d
Dmitry <kudim@inbox.ru>
2026-05-04 20:16:03 +0300

    feat: add Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Movie Search</h1>
+        <hr />
+      </div>
+    );
+  }
+}
+
+export default Header;
